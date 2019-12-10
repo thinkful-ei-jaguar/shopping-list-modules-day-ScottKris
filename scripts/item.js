1fe15caf2e46891acd167b9af2c4cbb996c92c7b
Scott Lingner <slingner@gmail.com>
2019-12-09 16:50:30 -0800

    just checking gh connection

--- scripts/item.js
+++ scripts/item.js
@@ -4,19 +4,19 @@
 //We will run the validateName function whenever 
 //we create an item or update its name, to prevent items from having blank names.
 let validateName = function(name) {
-    if(name === '') {
-      throw new TypeError ('Name must not be blank');
-    }
-  };
+  if(name === '') {
+    throw new TypeError ('Name must not be blank');
+  }
+};
   
-  let create = function(name) {
-    let item = {
-      id: cuid(),
-      name: name,
-      checked: false,
-    };
-    return item;
+let create = function(name) {
+  let item = {
+    id: cuid(),
+    name: name,
+    checked: false,
   };
+  return item;
+};
   
-  export default {validateName, create};
+export default {validateName, create};
   
